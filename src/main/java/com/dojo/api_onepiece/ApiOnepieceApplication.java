e2f97d4ebf01585bd6e8045fac08c26692b9b705
Brenda Nascimento <brendantrabalhos@gmail.com>
2025-11-22 16:41:32 -0300

    first commit

---
+++ src/main/java/com/dojo/api_onepiece/ApiOnepieceApplication.java
@@ -0,0 +1,13 @@
+package com.dojo.api_onepiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOnepieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOnepieceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dojo/api_onepiece/ApiOnepieceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.api_onepiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOnepieceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
