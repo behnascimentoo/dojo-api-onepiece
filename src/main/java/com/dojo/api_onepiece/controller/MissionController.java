e70e8a3398604347389e0ce8ed41256d9358d51b
Brenda Nascimento <brendantrabalhos@gmail.com>
2025-11-22 21:19:26 -0300

    mission controller

--- src/main/java/com/dojo/api_onepiece/controller/MissionController.java
+++ src/main/java/com/dojo/api_onepiece/controller/MissionController.java
@@ -1,19 +1,49 @@
 package com.dojo.api_onepiece.controller;
 
+import com.dojo.api_onepiece.dto.CreateMissionDto;
+import com.dojo.api_onepiece.dto.ToMissionResponseDto;
+import com.dojo.api_onepiece.dto.UpdateMissionDto;
 import com.dojo.api_onepiece.service.MissionService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Controller
+@RequestMapping("mission")
 public class MissionController {
 
     private final MissionService missionService;
 
-
     //Criar Missão
-    //Atualizar Missão
-    //Buscar Missão por id
-    //Listar todas as Missões
+    @PostMapping
+    public ResponseEntity<Void> createMission(@RequestBody CreateMissionDto createMissionDto) {
+        missionService.saveMission(createMissionDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
+    //Atualizar Missão
+    @PutMapping("/{id}")
+    public ResponseEntity<ToMissionResponseDto> updateMisson(@PathVariable Long id,@RequestBody UpdateMissionDto updateMissionDto){
+        ToMissionResponseDto updateMission = missionService.updateMission(id,updateMissionDto);
+        return ResponseEntity.ok(updateMission);
+    }
+
+    //Buscar Missão por id
+    @GetMapping("/{id}")
+    public ResponseEntity<ToMissionResponseDto> searchById(@PathVariable Long id){
+        ToMissionResponseDto mission = missionService.getMissionById(id);
+        return ResponseEntity.ok(mission);
+    }
+
+    //Listar todas as Missões
+    @GetMapping()
+    public ResponseEntity<List<ToMissionResponseDto>> listMissions() {
+        List<ToMissionResponseDto> missions = missionService.listOfAllMissions();
+        return ResponseEntity.ok(missions);
+    }
 }
